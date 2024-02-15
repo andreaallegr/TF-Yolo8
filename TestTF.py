3bb86e5ed2816ebd4c474c8bcabf1b8216109ee3
maogaviraghi <135248062+maogaviraghi@users.noreply.github.com>
2024-02-15 13:24:33 +0100

    Test Installazione Tensorflow

--- TestTF.py
+++ TestTF.py
@@ -1,6 +1,16 @@
 
 
 import tensorflow as tf
+import warnings
+import logging
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+
+warnings.filterwarnings("ignore")
+logging.getLogger().setLevel(logging.ERROR)
+tf.get_logger().setLevel(logging.ERROR)
+
 
 
 print(tf.config.list_physical_devices('GPU'))
@@ -14,6 +24,19 @@ print("is_cuda_gpu_available", is_cuda_gpu_available)
 
 is_cuda_gpu_min_3 = tf.test.is_gpu_available(True, (3,0))
 print("is_cuda_gpu_min_3", is_cuda_gpu_min_3)
+
+
+
+with tf.device('/gpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+
+
+print(c)
+
+print("TF Version:",tf.__version__)
+
 
 
 
